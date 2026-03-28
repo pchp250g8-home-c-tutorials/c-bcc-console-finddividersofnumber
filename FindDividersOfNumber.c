c569d3a5684784ca3209bbe2f0ee6492af95d108
Alexander <pchp250g8@tutamail.com>
2026-03-28 17:15:40 +0300

    Fixed Source Code

--- FindDividersOfNumber.c
+++ FindDividersOfNumber.c
@@ -6,6 +6,7 @@ int _tmain(int argc, _TCHAR* argv[])
     unsigned long ulNumber;
     printf("Input an integer positive number\r\n");
     scanf("%lu", &ulNumber);
+    printf("The number %lu has the following divisors:\r\n", ulNumber);
     for (unsigned long i = 1; i <= ulNumber; i++)
     {
         if (ulNumber % i == 0)
